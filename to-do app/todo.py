94298e9ea470d52383f0f7c5671b6bcae3598515
twennb <tom.wennberg@hotmail.com>
2025-03-11 15:10:18 +0100

    Initial implementation of the to-do list app structure

---
+++ to-do app/todo.py
@@ -0,0 +1,36 @@
+"""A simple CLI based to-do list app"""
+# Features:
+#  Add task
+#  Remove task
+#  View tasks
+#  Exit program
+#
+#  app should be constantly prompting the user for input
+#  until they choose to exit the app
+
+tasks = []
+
+
+def add_task(task):
+    """function to add tasks to list"""
+    pass
+
+
+def remove_task(task):
+    """function to remove tasks from list"""
+    pass
+
+
+def view_tasks():
+    """function to view tasks currently in list"""
+    pass
+
+
+def exit_program():
+    """function to exit the app"""
+    pass
+
+
+def main():
+    """main app function"""
+    pass
