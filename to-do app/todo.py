fde451136e34207b9fdeda0c1feff9f4218c8e73
twennb <tom.wennberg@hotmail.com>
2025-03-11 15:30:32 +0100

    filled in main() with core functionality

--- to-do app/todo.py
+++ to-do app/todo.py
@@ -8,29 +8,49 @@
 #  app should be constantly prompting the user for input
 #  until they choose to exit the app
 
-tasks = []
+task_list = []
 
 
 def add_task(task):
     """function to add tasks to list"""
-    pass
+    print(f"\nAdding task '{task}'\n")
 
 
 def remove_task(task):
     """function to remove tasks from list"""
-    pass
+    print(f"\nRemoving task '{task}'\n")
 
 
 def view_tasks():
     """function to view tasks currently in list"""
-    pass
+    print(f"\nCurrent tasks: {task_list}\n")
 
 
 def exit_program():
     """function to exit the app"""
-    pass
+    print("\nExiting the program...\n")
 
 
 def main():
     """main app function"""
-    pass
+    while True:
+        choice = input(
+            "Welcome to the To-Do list!\n"
+            "You can type:\n"
+            "'add' to add a task\n"
+            "'remove' to remove a task\n"
+            "'view' to see your tasks\n"
+            "'exit' to close the app\n"
+            "What would you like to do: "
+        )
+
+        choice = choice.lower()
+        match(choice.strip()):
+            case "add":
+                add_task(choice)
+            case "remove":
+                remove_task(choice)
+            case "view":
+                view_tasks()
+            case "exit":
+                exit_program()
